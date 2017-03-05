e0550ead981d5bb10fb03dbc160b62ddae0e8b80
Cristian PC <Cristian PC@192.168.8.100>
2017-03-05 10:23:20 -0600

    informacion de empresa

---
+++ controladores/ctrl_informacion_empresa.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   
+    
+});
+function ingresar_data(){
+  var n_instituto = $("#Nombre").val()
+  var telefono = $("#Telefono").val()
+  var mail = $("#Mail").val()
+  var n_instituto = $("#Nombre").val()
+  var dirección = $("#Nombre").val()
+    
+}
+
+
