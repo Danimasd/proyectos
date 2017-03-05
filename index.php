ab224d01734cdd34b905590f3fa29bfc83e12f17
Daniel <Daniel@DESKTOP-OTGUPIS.mshome.net>
2017-03-04 19:24:34 -0600

    conexion

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con=mysqli_connect("192.168.137.85","Correct Username","Correct Pass","Correct DB");
-if (mysqli_connect_errno())
-{
-echo "Fai led to connect to MySQL: " . mysqli_connect_error();//////
-}
-
-
