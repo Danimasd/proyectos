2212b53ae5f74c513f6dca0e7aa400481c0b71e2
Daniel <Daniel@DESKTOP-OTGUPIS.mshome.net>
2017-03-04 19:05:08 -0600

    funciones

---
+++ funciones/conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+$url = "localhost";
+$usuario = "root";
+$clave = "";
+$db = "inventario";
+$conexion = mysqli_connect($url, $usuario, $clave, $db);
+if ($conexion) {
+    echo 1;
+} else {
+    echo 2;
+}
