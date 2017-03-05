f56da380c792984f29b96ca52e79a65b7b5e0c58
Cristian PC <Cristian PC@192.168.8.100>
2017-03-04 18:58:25 -0600

    VARIAS LINEAS DE COMENTARIO

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $con=mysqli_connect("192.168.137.85","Correct Username","Correct Pass","Correct DB");
 if (mysqli_connect_errno())
 {
-echo "Fai led to connect to MySQL: " . mysqli_connect_error();
+echo "Fai led to connect to MySQL: " . mysqli_connect_error();//////
 }
 
 
